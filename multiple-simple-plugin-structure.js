59982d8d402e28113c315ee9b9c74972706a3245
Panos Ioannidis <panos277@hotmail.com>
2016-12-15 09:44:39 +0200

    multiple initialization simple plugin structure

---
+++ multiple-simple-plugin-structure.js
@@ -0,0 +1,36 @@
+(function($, window, document, undefined) {
+
+    // Choose your plugin name and the default data
+    var pluginName = "myNewPlugin",
+        defaults = {
+            key: "value"
+        };
+
+    function NewPlugin( element, options) {
+
+        this._defaults      = defaults;
+        this._pluginName    = pluginName;
+        this.$el            = element;
+
+        // Combine defaults and options to create a new object
+        this.options        = $.extend({}, this._defaults, options);
+
+        // Plugin initialization
+        this._init();
+
+    }
+
+    NewPlugin.prototype._init = function() {
+        //Add here your initialization code
+        // e.g alert(1);
+    };
+
+    // Register your new plugin to jQuery proto
+    $.fn[pluginName] = function( options ) {
+
+        return this.each( function() {
+                $.data(this, pluginName , new NewPlugin( this, options));
+        });
+    }
+
+})(jQuery, window, document);
